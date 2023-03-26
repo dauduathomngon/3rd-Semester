b47d9a18f81121bef7ac288c89d8fd2c7887d6c8
Lê Nguyễn <lenguyen18072003@gmail.com>
2023-03-26 10:51:37 +0700

    update

---
+++ Uni-DB/deleteTable.sql
@@ -0,0 +1,8 @@
+DECLARE @dropAllConstraints NVARCHAR(MAX) = N'';
+
+SELECT @dropAllConstraints  += N'
+ALTER TABLE ' + QUOTENAME(OBJECT_SCHEMA_NAME(parent_object_id))
+    + '.' + QUOTENAME(OBJECT_NAME(parent_object_id)) + 
+    ' DROP CONSTRAINT ' + QUOTENAME(name) + ';'
+FROM sys.foreign_keys;
+EXEC sp_executesql @dropAllConstraints 
